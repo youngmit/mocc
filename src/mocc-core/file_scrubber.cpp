c80880790a528b2c7051bc4131ace264cfbf2aae
Mitchell T.H. Young <youngmit@outlook.com>
2015-07-28 13:06:43 -0400

    add FileScrubber to the mocc namespace

--- src/mocc-core/file_scrubber.cpp
+++ src/mocc-core/file_scrubber.cpp
@@ -8,34 +8,35 @@
 
 using std::ifstream;
 
-
-FileScrubber::FileScrubber(const char* fName, const char* commentFlag):
-	stream_(fName),
-	flag_(commentFlag){
-   
-        if( !stream_.good() ) {
-            Error("Failed to open file.");
+namespace mocc {
+    FileScrubber::FileScrubber(const char* fName, const char* commentFlag):
+    	stream_(fName),
+    	flag_(commentFlag){
+       
+            if( !stream_.good() ) {
+                Error("Failed to open file.");
+            }
+        
         }
+    	
+    FileScrubber::~FileScrubber(){}
     
+    std::string FileScrubber::getline(){
+    	while(!stream_.eof()){
+    		std::string line;
+    		std::getline(stream_, line);
+    		// Strip the comments
+    		int commentPos = line.find(flag_, 0);
+    		if(commentPos != std::string::npos){
+    			line.erase(commentPos, std::string::npos);
+    		}
+    		// Remove whitespace
+    		line = trim(line);
+    		// If the result isnt empty, return the line.
+    		if(!line.empty()){
+    			return line;
+    		}
+    	}
+    	return "";
     }
-	
-FileScrubber::~FileScrubber(){}
-
-std::string FileScrubber::getline(){
-	while(!stream_.eof()){
-		std::string line;
-		std::getline(stream_, line);
-		// Strip the comments
-		int commentPos = line.find(flag_, 0);
-		if(commentPos != std::string::npos){
-			line.erase(commentPos, std::string::npos);
-		}
-		// Remove whitespace
-		line = trim(line);
-		// If the result isnt empty, return the line.
-		if(!line.empty()){
-			return line;
-		}
-	}
-	return "";
 }
