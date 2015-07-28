194590a61e26a7c53fc3327c0e76483cfcd911f4
Mitchell T.H. Young <youngmit@outlook.com>
2015-07-28 14:15:49 -0400

    Add partial unit test for Material/MaterialLib
    A very basic test was added for the Material and MaterialLib classes. It pretty
    much just spot-checks the scattering stuff, since its the easiest to screw up.

---
+++ src/mocc-core/tests/test_material.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+
+#include "global_config.hpp"
+#include "file_scrubber.hpp"
+#include "material_lib.hpp"
+#include "material.hpp"
+#include "fp_utils.hpp"
+
+using std::cout;
+using std::endl;
+
+int main() {
+    mocc::FileScrubber c5g7_file ("c5g7.xsl", "!");
+    mocc::MaterialLib matlib(c5g7_file);
+
+    matlib.assignID( 1, "MOX-4.3");
+
+    const mocc::Material* mat = matlib.materials().at(1);
+
+    assert(mat->xsab().size() == 7);
+
+
+    mocc::VecF out_scat { 1.702972340405E-01,
+                          3.270915015982E-01,
+                          4.558022000000E-01,
+                          4.627124000000E-01,
+                          2.862871691656E-01,
+                          2.698171000000E-01,
+                          2.735018000000E-01 };
+
+
+
+    cout << endl << endl;
+    for( int ig=0; ig<7; ig++ ) {
+        assert(mocc::fp_equiv_ulp( mat->xssc().out(ig), out_scat[ig]) );
+
+
+        
+        const mocc::ScatRow& scat_row = mat->xssc().to(ig);
+        unsigned int min_g = scat_row.min_g;
+        unsigned int max_g = scat_row.max_g;
+        for( int igg=min_g; igg<=max_g; igg++ ) {
+            cout << scat_row.from[igg-min_g] << " ";
+        }
+
+        cout << endl;
+    }
+    assert(mat->xssc().to(3).from[0] == 5.04050E-09);
+}
