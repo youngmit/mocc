7af464943b27106a50158e0f6fceb0217b11f770
Mitchell T.H. Young <youngmit@outlook.com>
2015-07-31 19:15:22 -0400

    Added a method to H5 file to write scalar ints

--- src/mocc-core/h5file.cpp
+++ src/mocc-core/h5file.cpp
@@ -17,5 +17,24 @@ namespace mocc {
         H5::DataSet dataset = file_.createDataSet(path, 
                 H5::PredType::NATIVE_DOUBLE, space);
         dataset.write(data.data(), H5::PredType::NATIVE_DOUBLE);
+
+        return;
+    }
+
+    void H5File::write( std::string path, int data ) {
+        hsize_t dims_a[1];
+
+        dims_a[0] = 1;
+
+        H5::DataSpace space( 1, dims_a );
+        H5::DataSet dataset = file_.createDataSet(path, 
+                H5::PredType::NATIVE_INT, space);
+        dataset.write(&data, H5::PredType::NATIVE_INT);
+
+        return;
+    }
+
+    UP_Group_t H5File::mkdir( std::string path ) {
+        return UP_Group_t( new H5::Group( file_.createGroup( path ) ) );
     }
 }
--- src/mocc-core/h5file.hpp
+++ src/mocc-core/h5file.hpp
@@ -2,14 +2,21 @@
 
 #include <string>
 #include <H5Cpp.h>
+#include <memory>
 
 #include "global_config.hpp"
 
 namespace mocc {
+    // Useful typedefs
+    typedef std::unique_ptr<H5::Group> UP_Group_t;
+
+
     class H5File {
     public:
         H5File(std::string fname);
         void write( std::string path, VecF data, VecI dims );
+        void write( std::string path, int data );
+        UP_Group_t mkdir( std::string path );
     private:
         H5::H5File file_;
     };
