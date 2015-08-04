a3b2ef6b72d7638394a6c5512994086ff73208f2
Mitchell T.H. Young <youngmit@outlook.com>
2015-08-04 16:44:10 -0400

    Add Exception class and related macro
    Thought it might be good to start using proper exception handling, so added a
    simple Exception class that prints the location (file, line, function) in which
    it was thrown, a macro to make it easier to generate and a Fail function to give
    up.

--- src/mocc-core/error.hpp
+++ src/mocc-core/error.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <iostream>
+#include <exception>
 #include <stdlib.h>
 #include <string>
+#include <sstream>
 
 using std::cout;
 using std::endl;
@@ -11,6 +13,42 @@ static void Error(const char* msg){
 	cout << "ERROR: " << msg << endl;
 	exit(EXIT_FAILURE);
 }
+
+
 static void Warn(const char* msg){
 	cout << "WARNING: " << msg << endl;
+}
+
+namespace mocc {
+    class Exception: public std::exception {
+    public:
+        Exception( const char* file, int line, const char* func, const char* msg ):
+            file_( file ),
+            line_( line ),
+            func_( func ),
+            message_( msg )
+        {
+            return;
+        }
+        const char* what() const noexcept {
+            std::stringstream ret;
+            ret << file_ << ":" << line_ << " in " << func_ << endl;
+            ret << message_ << endl;
+            std::cout << ret.str();
+            return ret.str().c_str();
+        }
+    private:
+        std::string file_;
+        int line_;
+        std::string func_;
+        std::string message_;
+    };
+
+    static void Fail( mocc::Exception e ) {
+        cout << e.what();
+        exit(EXIT_FAILURE);
+    }
+
+
+#define EXCEPT(msg) Exception(__FILE__, __LINE__, __func__, msg);
 }
