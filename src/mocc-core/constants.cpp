a31b3c921fefa9e0d8036e981f3fd25c57b2ee12
Mitchell T.H. Young <youngmit@outlook.com>
2015-09-09 11:44:34 -0400

    Remove extern keywords from constants.cpp

--- src/mocc-core/constants.cpp
+++ src/mocc-core/constants.cpp
@@ -1,13 +1,13 @@
 #include "constants.hpp"
 
 namespace mocc {
-extern const Surface AllSurfaces[] = { Surface::EAST, 
+const Surface AllSurfaces[] = { Surface::EAST, 
                                            Surface::NORTH, 
                                            Surface::WEST, 
                                            Surface::SOUTH, 
                                            Surface::TOP, 
                                            Surface::BOTTOM };
 
-extern const Normal AllNormals[] = { Normal::X_NORM, Normal::Y_NORM,
+const Normal AllNormals[] = { Normal::X_NORM, Normal::Y_NORM,
     Normal::Z_NORM };
 }
