7b3b1b735506914ad553e0bdc4d8b3f8b25039e6
Mitchell T.H. Young <youngmit@outlook.com>
2015-09-15 09:26:45 -0400

    Add some doc to eigen interface

--- src/mocc-core/eigen_interface.hpp
+++ src/mocc-core/eigen_interface.hpp
@@ -10,9 +10,17 @@
 namespace mocc {
 #ifdef FORCE_SINGLE
 typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> MatrixX;
-typedef Eigen::Array<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> ArrayX;
 #else
 typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> MatrixX;
+#endif
+
+/**
+ * This typedef provides easy access to the Eigen Array class, templated to the
+ * appropriate floating point data type, and to use column-major ordering.
+ */
+#ifdef FORCE_SINGLE
+typedef Eigen::Array<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> ArrayX;
+#else
 typedef Eigen::Array<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> ArrayX;
 #endif
 
