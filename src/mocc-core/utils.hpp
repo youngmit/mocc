6fc5a25bb918904229620adb7fd583bf77b694f7
Mitchell Young <mitchelly@gmail.com>
2015-10-13 15:24:18 -0400

    Add a utils header with Normalize routine
    For lack of a better place to put it, i created a utils.hpp file to store a
    Normalize() function, which accepts a range and normalizes it to sum to the
    number of non-zero elements in the range.

---
+++ src/mocc-core/utils.hpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iterator>
+
+namespace mocc {
+    /**
+     * Normalize the range of values in [first, last). The normalization
+     * guarantees that the values will sum to the number of non-zero entries in
+     * the range.
+     */
+    template <class InputIterator>
+    void Normalize( InputIterator first, InputIterator last ) {
+        typedef typename std::iterator_traits<InputIterator>::value_type T;
+
+        // Count the number of elements greater than zero
+        int n = 0;
+
+        T sum = 0.0;
+        for( auto it=first; it!=last; ++it ) {
+            if( *it > 0.0 ) {
+                n++;
+            }
+            sum += *it;
+        }
+
+        T f = (T)n/sum;
+
+        for( auto it=first; it!=last; ++it ) {
+            *it *= f;
+        }
+
+    }
+
+}
