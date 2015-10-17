55d7dcefc816b2adb643a9dfab1451227f5d8ea8
Mitchell T.H. Young <youngmit@outlook.com>
2015-10-16 20:04:48 -0400

    Add some more docs to the HDF5 code

--- src/mocc-core/h5file.hpp
+++ src/mocc-core/h5file.hpp
@@ -7,6 +7,13 @@
 #include "global_config.hpp"
 
 namespace mocc {
+    /**
+     * \section hdf5_dimensions HDF5 Dataset Dimensions
+     * The dimensions of the HDF5 datasets should be reversed. This is a legacy
+     * carryover from my plotting software being used to interacting with data
+     * from Fortran code. Make sure to flip the order of the dimensions when
+     * performing output.
+     */
     namespace HDF {
         /**
          * Write a vector of floats to the HDF5 file at the specified location
@@ -19,6 +26,7 @@ namespace mocc {
          * \param data the vector of data. The data is read 1-dimensional, but
          * resized to the dimensions specified by \c dims.
          * \param dims a vector of ints containing the dimensions of the data
+         * (see \ref hdf5_dimensions )
          */
         void Write( H5::CommonFG *node, std::string path, VecF data, 
                 VecI dims );
@@ -45,12 +53,22 @@ namespace mocc {
          * \param first an iterator pointing to the beginning of the data to be
          * written.
          * \param last an iterator past the end of the data to be written.
+         * \param dims a vector of ints containing the dimensions of the data
+         * (see \ref hdf5_dimensions )
          */
         VecF::const_iterator Write( H5::CommonFG *node, std::string path, 
                 VecF::const_iterator first,
                 VecF::const_iterator last,
                 VecI dims );
 
+        /**
+         * This is a very simple wrapper for the HDF5 file class, which
+         * essentially just helps with opening a file, given a path, and can
+         * return a H5::ComonFG to the root of the file.
+         *
+         * To write to the file (or any other reference to a H5::CommonFG) use
+         * the HDF::Write functions above.
+         */
         class H5File {
         public:
             H5File( std::string fname );
