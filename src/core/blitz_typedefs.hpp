3004efebfa2757cb7402b3d0358286bb3661c068
Jipu Wang <jipuwang@umich.edu>
2016-02-23 13:56:04 -0500

    Added global function DotProduct that gives dot product of two ArrayB1 objects.

--- src/core/blitz_typedefs.hpp
+++ src/core/blitz_typedefs.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <blitz/array.h>
+#include <cassert>
 
 namespace mocc {
 
@@ -15,5 +16,14 @@ typedef blitz::Array<double, 2> ArrayB2;
 typedef blitz::Array<double, 3> ArrayB3;
 typedef blitz::Array<double, 4> ArrayB4;
 #endif
+
+real_t DotProduct( const ArrayB1 &ary1, const ArrayB1 &ary2 ) {
+    real_t result=0.0;
+    assert( ary1.size() == ary2.size() );
+    for( int i=0; i<ary1.size(); i++ ) {
+        result = result+ary1(i)*ary2(i);
+    }
+    return result;
+}
 
 }
