93e72437eda37f6e3164b796e9e27a92f0427052
Jipu Wang <jipuwang@umich.edu>
2016-03-05 17:04:21 -0500

    added test for ChebyYamamoto quad set and ChebyGauss quad set. But the isValidOutput still fails due to problems specified in the comment

--- src/core/tests/test_angular_quadrature.cpp
+++ src/core/tests/test_angular_quadrature.cpp
@@ -8,6 +8,7 @@
 #include "pugixml.hpp"
 
 #include "angular_quadrature.hpp"
+#include "product_quadrature.hpp"
 
 using std::cout;
 using std::endl;
@@ -95,10 +96,23 @@ public:
     }
 };
 
+class ChebyshevGauss_16_3 : public AngQuadFixture {
+public:
+    ChebyshevGauss_16_3() {
+        this->make_angquad("<ang_quad type=\"cg\" azimuthal-order=\"16\" polar-order=\"3\" />");
+    }
+};
+
+class ChebyshevYamamoto_16_3 : public AngQuadFixture {
+public:
+    ChebyshevYamamoto_16_3() {
+        this->make_angquad("<ang_quad type=\"cy\" azimuthal-order=\"16\" polar-order=\"3\" />");
+    }
+};
    
 TEST_FIXTURE( LevelSymmetric_4, general )
 {
-    cout << ang_quad << endl;
+    //cout << ang_quad << endl;
 
     // Test the angle reflection capabilities
     test_reflect();
@@ -117,31 +131,75 @@ TEST_FIXTURE( LevelSymmetric_4, general )
 
 TEST_FIXTURE( LevelSymmetric_6, higher_order ) {
     
+    // Test the angle reflection capabilities
+    test_reflect();
+
+    // Other tests
+    CHECK_EQUAL(6, ang_quad.ndir_oct());
     // Test the weight sum is 8.0
     CHECK_CLOSE(8.0, total_weight(), 0.00000000000001);
     // Test input/output
     CHECK(isValidOutput());
-    
 }
 
 
-TEST_FIXTURE( AngQuadFixture, Chebyshev16Gauss3 ) {
-    std::string inp = "<ang_quad type=\"cg\" azimuthal-order=\"16\" polar-order=\"3\" />";
-    make_angquad( inp );
-    // Test the weight sum is 8.0
-   // CHECK_CLOSE(8.0, total_weight(), 0.00000000000001);
-
-}
-
-/*
-TEST_FIXTURE( AngQuadFixture, Chebyshev16Yamamoto3 ) {
+TEST_FIXTURE( ChebyshevYamamoto_16_3, cy_general ) {
     std::string inp = "<ang_quad type=\"cy\" azimuthal-order=\"16\" polar-order=\"3\" />";
-    make_angquad( inp );
+    // Test the angle reflection capabilities
+    test_reflect();
+
+    // Other tests
+    CHECK_EQUAL(48, ang_quad.ndir_oct());
     // Test the weight sum is 8.0
-//    CHECK_CLOSE(8.0, total_weight(), 0.00000000000001);
+    CHECK_CLOSE(8.0, total_weight(), 0.00000000000001);
+/**
+ * \todo isValidOutput() fails when Angle constructor used to generate the
+ * output angular quadrature is different from the Angle constructor used in the
+ * constructor that accepts a h5file node.
+ */
+    // Test input/output
+    // CHECK(isValidOutput());
+    // Test the first angle
+    CHECK_CLOSE(0.049087385212340, ang_quad[0].alpha,    0.0000000000001); 
+    CHECK_CLOSE(0.167429147795000, ang_quad[0].theta,    0.0000000000001); 
+    CHECK_CLOSE(0.166447265186000, ang_quad[0].ox,       0.0000000000001); 
+    CHECK_CLOSE(0.008177029791330, ang_quad[0].oy,       0.0000000000001); 
+    CHECK_CLOSE(0.986016452244020, ang_quad[0].oz,       0.0000000000001); 
+    CHECK_CLOSE(0.002889562500000, ang_quad[0].weight,   0.0000000000001); 
+    CHECK_CLOSE(6.000672075260800, ang_quad[0].rsintheta,0.0000000000001); 
 }
-*/
- 
+
+
+TEST_FIXTURE( ChebyshevGauss_16_3, cg_general ) {
+    
+    //cout << ang_quad << endl;
+    // Test the angle reflection capabilities
+    test_reflect();
+
+    // Other tests
+    CHECK_EQUAL(48, ang_quad.ndir_oct());
+    // Test the weight sum is 8.0
+    CHECK_CLOSE(8.0, total_weight(), 0.0000000000001);
+    // Test the first angle
+    CHECK_CLOSE(0.049087385212340, ang_quad[0].alpha,    0.000000000001); 
+    CHECK_CLOSE(0.374822141002272, ang_quad[0].theta,    0.000000000001); 
+    CHECK_CLOSE(0.365666032196437, ang_quad[0].ox,       0.000000000001); 
+    CHECK_CLOSE(0.017964020229512, ang_quad[0].oy,       0.000000000001); 
+    CHECK_CLOSE(0.930572752059133, ang_quad[0].oz,       0.000000000001); 
+    CHECK_CLOSE(0.029244620910793, ang_quad[0].weight,   0.000000000001); 
+    CHECK_CLOSE(2.731441720757410, ang_quad[0].rsintheta,0.000000000001); 
+  
+    // Test input/output
+    // CHECK(isValidOutput());
+}
+
+
 int main() {
     return UnitTest::RunAllTests();
 }
+
+
+
+
+
+
