a6c3728943778b7d8bd5c1f6860e50a356415df4
Mitchell T.H. Young <youngmit@outlook.com>
2016-03-09 13:29:16 -0500

    Remove the single-precision overload for fp_equiv_ulp
    Only having one allows to use it as a predicate. If i were to template and
    specialize for single precision, we could get away with both, but for now, who
    cares

--- src/core/fp_utils.hpp
+++ src/core/fp_utils.hpp
@@ -26,28 +26,7 @@ namespace mocc {
      * href="https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/">
      * this page</a> for more info about testing floating point equivalence.
      */
-    inline bool fp_equiv_ulp(float v1, float v2) {
-        fp_utils::float_int i1;
-        i1.f = v1;
-        if (i1.i < 0) {
-            i1.i = 0x80000000 - i1.i;
-        }
-
-        fp_utils::float_int i2;
-        i2.f = v2;
-        if (i2.i < 0) {
-            i2.i = 0x80000000 - i2.i;
-        }
-
-        return std::abs(i1.i - i2.i) < 100;
-    }
-
-    /**
-     * \brief Compare two doubles using ULP.
-     *
-     * \copydetails fp_equiv_ulp(float, float)
-     */
-    inline bool fp_equiv_ulp(double v1, double v2) {
+    inline bool fp_equiv_ulp(real_t v1, real_t v2) {
         fp_utils::double_int i1;
         i1.f = v1;
         if (i1.i < 0) {
@@ -60,7 +39,7 @@ namespace mocc {
             i2.i = 0x80000000 - i2.i;
         }
 
-        return std::abs(i1.i - i2.i) < 100;
+        return std::abs(i1.i - i2.i) < 200;
     }
 
     inline bool fp_equiv_rel(real_t v1, real_t v2) {
