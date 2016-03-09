520a258c47b809cfc4eaeef4cdaecdf8e575ba5c
Mitchell T.H. Young <youngmit@outlook.com>
2016-03-09 13:46:29 -0500

    Add message stating which type of sweeper in Factory

--- src/sweepers/transport_sweeper_factory.cpp
+++ src/sweepers/transport_sweeper_factory.cpp
@@ -21,17 +21,21 @@ namespace mocc {
         // Check the input XML for which type of sweeper to make
         std::string type = input.child("sweeper").attribute("type").value();
         if( type == "moc" ) {
+            LogScreen << "Using an MoC sweeper" << std::endl;
             UP_Sweeper_t ts( new moc::MoCSweeper( input.child("sweeper"),
                         mesh ) );
             sweeper = std::move( ts );
         } else if ( type == "sn" ) {
+            LogScreen << "Using an Sn sweeper" << std::endl;
             auto snts = SnSweeperFactory( input.child("sweeper"), mesh );
             sweeper = std::move(snts);
         } else if ( type == "2d3d" ) {
+            LogScreen << "Using a 2D3D wweeper" << std::endl;
             UP_Sweeper_t ts( new cmdo::PlaneSweeper_2D3D( input.child("sweeper"),
                         mesh) );
             sweeper = std::move( ts );
         } else if ( type == "moc_2d3d" ) {
+            LogScreen << "Using a standalone 2D3D MoC sweeper" << std::endl;
             // Create a 2D3D MoC sweeper by itself. This is really only useful
             // for one-way coupling
             cmdo::MoCSweeper_2D3D *swp = new cmdo::MoCSweeper_2D3D(
