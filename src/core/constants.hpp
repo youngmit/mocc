9110957871c71ffbbfe73925bb50308b90b93505
Mitchell T.H. Young <youngmit@outlook.com>
2016-03-09 13:32:47 -0500

    Protect all of the preprocessor defined constants with ()
    all sorts of terrifying order-of-operations nastiness could happen there...

--- src/core/constants.hpp
+++ src/core/constants.hpp
@@ -3,12 +3,12 @@
 #include <iostream>
 
 #define PI 3.1415926535897932
-#define TWOPI 2.0*PI
-#define HPI 0.5*PI
-#define RPI 1.0/PI
-#define RTWOPI 1.0/TWOPI
-#define FPI 4.0*PI
-#define RFPI 1.0/(FPI)
+#define TWOPI (2.0*PI)
+#define HPI (0.5*PI)
+#define RPI (1.0/PI)
+#define RTWOPI (1.0/TWOPI)
+#define FPI (4.0*PI)
+#define RFPI (1.0/FPI)
 
 namespace mocc {
     // Surface and direction indexing
