d9630273626749ff390690a5b3f53487326758db
Mitchell T.H. Young <youngmit@outlook.com>
2016-03-27 16:09:40 -0400

    Fix some documentation issues`

--- src/sweepers/cmdo/sn_sweeper_factory_cdd.hpp
+++ src/sweepers/cmdo/sn_sweeper_factory_cdd.hpp
@@ -20,12 +20,12 @@ namespace mocc { namespace cmdo {
      * the input provided.
      *
      * This factory is responsible for interpreting the provided input to
-     * determine and create the appropriate \ref SnSweeper and return it. This
-     * factory is distinct from the vanilla \ref SnSweeperFactory in that it
-     * also creates \ref CorrectionData for the sweeper and returns it as well.
-     * This is important, because the sweeper that is ultimately returned from
-     * this function is of the base type \ref SnSweeper, which doesn't actually
-     * know anything about the existence of correction factors.
+     * determine and create the appropriate \ref sn::SnSweeper and return it.
+     * This factory is distinct from the vanilla \ref SnSweeperFactory in that
+     * it also creates \ref CorrectionData for the sweeper and returns it as
+     * well.  This is important, because the sweeper that is ultimately returned
+     * from this function is of the base type \ref sn::SnSweeper, which doesn't
+     * actually know anything about the existence of correction factors.
      *
      * \note While it would be possible to maintain type information about the
      * CDD nature for the returned sweeper, it would be necessary to propagate
@@ -37,10 +37,10 @@ namespace mocc { namespace cmdo {
      * returned from the factory, and \ref CorrectionData that it contains is
      * the \a only reference to the \ref CorrectionData that survives the to
      * the end of this function. It is also impossible to get a new one, since
-     * sufficient type information to do so is discarded when an \ref SnSweeper
-     * is returned. Moral of the story is to be careful with what you do with
-     * the return value of this function. Here is a simple example of how to
-     * screw up:
+     * sufficient type information to do so is discarded when an \ref
+     * sn::SnSweeper is returned. Moral of the story is to be careful with what
+     * you do with the return value of this function. Here is a simple example
+     * of how to screw up:
      * \code
      * std::unique_ptr<SnSweeper> sweeper = SnSweeperFactory_CDD( input, mesh).first;
      * std::shared_ptr<CorrectionData> corrections = SnSweeperFactory_CDD( input, mesh ).second;
