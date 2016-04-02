2c66631af7faeadf7a13fda00c9664cb65fadd7c
Mitchell Young <Mitchell Young>
2016-04-02 18:11:12 -0400

    Hide stacktrace code when WIN32

--- src/util/stacktrace.hpp
+++ src/util/stacktrace.hpp
@@ -1,24 +1,9 @@
-/*
-   Copyright 2016 Mitchell Young
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
 // stacktrace.h (c) 2008, Timo Bingmann from http://idlebox.net/
 // published under the WTFPL v2.0
 
-#ifndef _STACKTRACE_H_
-#define _STACKTRACE_H_
+#pragma once
+
+#ifndef WIN32
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -106,5 +91,10 @@ static inline void print_stacktrace(FILE *out = stderr, unsigned int max_frames 
     free(symbollist);
 }
 
-#endif // _STACKTRACE_H_
+#else
+static inline void print_stacktrace(FILE *out = stderr, unsigned int max_frames = 63) {
+	return;
+}
+
+#endif
 
