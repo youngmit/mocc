05d8af4d4658b19a41adae50e2b0dd77039db9fe
Jipu Wang <jipuwang@umich.edu>
2016-04-04 22:06:59 -0400

    change the ScatteringMatrix constructor such that it treats all-zero rows correctly: with the following behavior: 1. insert into scat_ a zero-valued element 2. set the ScatteringRow data member such that min_g=max_g=row index and the from pointer pointing to a one-element array with value zero.

--- src/core/scattering_matrix.cpp
+++ src/core/scattering_matrix.cpp
@@ -25,49 +25,58 @@ namespace mocc {
         // Imply ng_ from the size of the passed-in vectors
         ng_ = scat.size();
         out_ = VecF(ng_, 0.0);
+        rows_.reserve(ng_);
 
-        // Densify the scattering matrix
-        for( int to=0; to<ng_; to++ ) {
+        // Determine the size of scat_ and save the bounds for ScatteringRows
+        int size = 0;
+        std::vector<std::pair<int, int>> bounds;
+        bounds.reserve(ng_);
+        
+        int to=0;
+        for( const auto &scatRow: scat ) {
+            std::pair<int, int> these_bounds;
             for( int from=0; from<ng_; from++ ) {
-                if(scat[to][from] > 0.0){
-                    scat_.push_back(scat[to][from]);
-                }
-                out_[from] += scat[to][from];
-            }
-        }
-
-        // Determine group bounds for each row
-        // TODO: This is some nasty jazz, might have originally written this
-        // whilst under the influence of something... Come back and clean up
-        // later.
-        int pos = 0;
-        int prevPos = 0;
-        int min_g = 0;
-        int max_g = 0;
-        for( int to=0; to<ng_; to++ ) {
-            bool found_min = false;
-            bool found_max = false;
-            for( int from=0; from<ng_; from++ ) {
-                if( scat[to][from] > 0.0 ) {
-                    if(!found_min){
-                        found_min = true;
-                        min_g = from;
-                    }
-                    pos++;
-                }
-                if( scat[to][from] == 0.0 && found_min ) {
-                    found_max = true;
-                    max_g = from-1;
+                these_bounds.first = from;
+                if( scatRow[from] > 0.0 ) {
                     break;
                 }
             }
-            if( !found_max ) {
-                max_g = ng_-1;
+            for( int from=ng_-1; from>=0; from-- ) {
+                these_bounds.second = from;
+                if( scatRow[from] > 0.0 ) {
+                    break;
+                }
             }
-            rows_.push_back(ScatteringRow(min_g, max_g, &scat_[prevPos]));
-            prevPos = pos;
+            if( these_bounds.first==ng_-1 && these_bounds.second==0 ) {
+                these_bounds.first = to;
+                these_bounds.second = to;
+            }
+            size += these_bounds.second-these_bounds.first+1;
+            bounds.push_back(these_bounds);
+        
+            to++;
         }
-        return;
+
+        scat_.reserve(size);
+
+        auto *begin = scat_.data();
+
+        to=0;
+        int pos=0;
+        for( auto & these_bounds: bounds ) {
+            for ( int from=these_bounds.first; 
+                    from<=these_bounds.second; from++ ) {
+                scat_.push_back(scat[to][from]);
+                out_[from] += scat[to][from];
+            }
+            rows_.push_back(ScatteringRow(these_bounds.first,
+                        these_bounds.second, &scat_[pos]));
+            to++;
+            pos += these_bounds.second-these_bounds.first+1;
+        }
+        
+        // Check whether scat_ is reallocated
+        assert( begin == scat_.data() );
     }
 
     ScatteringMatrix::ScatteringMatrix(const ArrayB2 &scat){
