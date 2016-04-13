39b82bc5b5b2572e7b2e7b108af1608a6f9ccb34
Mitchell T.H. Young <youngmit@outlook.com>
2016-04-13 12:41:33 -0400

    Fix bug in python emitted by RayData

--- src/sweepers/moc/ray_data.cpp
+++ src/sweepers/moc/ray_data.cpp
@@ -421,8 +421,10 @@ namespace mocc { namespace moc {
                 os << ",    # " << r.bc(0) << " " << r.bc(1)  << endl;
             }
             // store the location before the comma
+            auto end_pos = os.tellp();
             os.seekp(ray_pos);
             os << " ]";
+            os.seekp(end_pos);
             angle_pos = os.tellp();
             os << "," << endl;
         }
