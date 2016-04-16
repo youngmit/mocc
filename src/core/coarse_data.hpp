c45006fd9ba9b3f9ce7d5ab826e813582baf3ba0
Mitchell T.H. Young <youngmit@outlook.com>
2016-04-16 12:50:24 -0400

    Add old partial current storage to CoarseData

--- src/core/coarse_data.hpp
+++ src/core/coarse_data.hpp
@@ -40,12 +40,14 @@ namespace mocc {
             current( (int)mesh.n_surf(), ngroup ),
             surface_flux( (int)mesh.n_surf(), ngroup ),
             partial_current( (int)mesh.n_surf(), ngroup ),
+            partial_current_old( (int)mesh.n_surf(), ngroup ),
             flux( (int)mesh.n_pin(), ngroup ),
             old_flux( (int)mesh.n_pin(), ngroup ),
             n_group_( ngroup ),
             mesh_(mesh),
             has_data_radial_( false ),
-            has_data_axial_( false )
+            has_data_axial_( false ),
+            has_old_partial_( false )
         {
             current = 0.0;
             surface_flux = 0.0;
@@ -61,22 +63,78 @@ namespace mocc {
             return;
         }
 
+        /**
+         * \brief Signal to other clients of the \ref CoarseData that data have
+         * been explicitly defined for the radial-facing surfaces (X- and
+         * Y-normal).
+         *
+         * Upon construction, this is set to \c false. When a sweeper or other
+         * client sets values on the \ref CoarseData, it should set it to true
+         * so that other clients know that they can use it, via the \ref
+         * has_radial_data() method.
+         *
+         * For example, the \ref CMFD solver should not try to calculate D-hats
+         * for surfaces unless currents have been supplied by a transport
+         * sweeper. In the context of a 2-D MoC sweeper, which never sets axial
+         * currents, the \ref CMFD solver should never calculate D-hats, even
+         * though the axial currents may be non-zero (since the \ref CMFD solver
+         * updats the currents after a solve).
+         */
         void set_has_radial_data( bool has ) {
             has_data_radial_ = has;
             return;
         }
 
+        /**
+         * \brief Signal to other clients of the \ref CoarseData that axial
+         * (Z-normal) currents have been explicitly defined.
+         *
+         * This is similar to \ref set_has_radial_data(), but for the axial
+         * surfaces. This should be set \c true after a 3-D sweeper has
+         * calculated currents.
+         */
         void set_has_axial_data( bool has ) {
             has_data_axial_ = has;
             return;
         }
 
+        /**
+         * \brief Return whether data have been explicitly defined for axial
+         * surfaces.
+         */
         bool has_axial_data() const {
             return has_data_axial_;
         }
 
+        /**
+         * \brief Return whether data have been explicitly defined for radial
+         * surfaces.
+         */
         bool has_radial_data() const {
             return has_data_radial_;
+        }
+
+        /**
+         * \brief Return whether previous-iteration values for partial currents
+         * are available.
+         *
+         * This starts as \c false at construction time, and is set to \c true
+         * immediately after the first \ref CMFD solve, or similar. This is
+         * necessary, since the logic for tasks like updating incomming flux is
+         * different if old values are available. See the various
+         * implementations of \ref TransportSweeper::update_incoming_flux().
+         */
+        bool has_old_partial() const {
+            return has_old_partial_;
+        }
+
+        /**
+         * \brief Signal to other clients of the \ref CoarseData that it has
+         * previous-iteration values for partial current.
+         */
+        void set_has_old_partial( bool has ) {
+            has_old_partial_ = has;
+            return;
         }
 
         /**
@@ -132,6 +190,7 @@ namespace mocc {
         ArrayB2 current;
         ArrayB2 surface_flux;
         blitz::Array<std::array<real_t, 2>, 2> partial_current;
+        blitz::Array<std::array<real_t, 2>, 2> partial_current_old;
         ArrayB2 flux;
         ArrayB2 old_flux;
     private:
@@ -139,6 +198,7 @@ namespace mocc {
         const Mesh &mesh_;
         bool has_data_radial_;
         bool has_data_axial_;
+        bool has_old_partial_;
     };
 
     typedef std::shared_ptr<CoarseData> SP_CoarseData_t;
