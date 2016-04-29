dad0b29003dc3786bc4becde10962c384ab51e8b
Mitchell T.H. Young <youngmit@outlook.com>
2016-04-29 14:29:30 -0400

    Add XML constructor for Angle

--- src/core/angle.hpp
+++ src/core/angle.hpp
@@ -19,6 +19,8 @@
 #include <cmath>
 #include <iosfwd>
 
+#include "pugifwd.hpp"
+
 #include "core/constants.hpp"
 #include "core/fp_utils.hpp"
 #include "core/global_config.hpp"
@@ -28,6 +30,34 @@ namespace mocc {
         return 180*(rad*RPI);
     }
 
+    /**
+     * The \ref Angle struct is used to represent a discrete direction in
+     * angular space, with an associated quadrature weight. An \ref Angle
+     * carries the direction represented as both an azimuthal angle,
+     * \f[
+     *     \alpha \in \left\{\left(0, 2\pi\right) \setminus
+     *     \left\{\frac{\pi}{2}, \pi, \frac{3\pi}{2}\right\}\right\}
+     *  \f]
+     * and polar angle,
+     * \f[
+     *     \theta \in \left(-\frac{\pi}{2}, \frac{\pi}{2}\right),
+     * \f]
+     * as well as their corresponding direction cosines,
+     * \f{eqnarray*}{
+     *     \Omega_x &=& \sqrt{1-\cos(\theta)^2}\cos\alpha, \\
+     *     \Omega_y &=& \sqrt{1-\cos(\theta)^2}\sin\alpha, \\
+     *     \Omega_z &=& \cos\theta.
+     * \f}
+     *
+     * The angles \f$\{\frac{\pi}{2}, \pi, \frac{3\pi}{2}\}\f$ are excluded from
+     * the set of possible azimuthal angles, since throughout the code it is
+     * assumed that all angles fall unambiguously within an octant; having
+     * an angle that lies on an axis would violate this assumption. This
+     * requirement renders it somewhat difficult to represent certain situations
+     * (e.g. a monodirectional beam oriented in the positive-X direction), but
+     * such situations are rare, and may still be modelled by an angle which
+     * lies very close to, but not directly on the axis.
+     */
     struct Angle {
         /// x-component of the angle
         real_t ox;
@@ -82,6 +112,18 @@ namespace mocc {
             return;
         }
 
+
+        /**
+         * Construct using input from an XML node
+         */
+        Angle( const pugi::xml_node &input );
+
+        /**
+         * \brief Return an Angle, which is the reflection of the current angle
+         * into the desired octant. 
+         *
+         * \param octant an integer in [1,8], specifying the desired octant.
+         */
         Angle to_octant( int octant ) const;
 
         // Provide stream insertion support
@@ -134,7 +176,7 @@ namespace mocc {
          * equivalent conditions must all be evaluated.
          */
         bool operator!=( const Angle &other ) const {
-            bool not_equal = 
+            bool not_equal =
             (
                 !fp_equiv_ulp( ox, other.ox ) ||
                 !fp_equiv_ulp( oy, other.oy ) ||
