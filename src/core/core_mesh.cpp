cc17428b8035c7628727ddcc3341666d00036c27
Mitchell T.H. Young <youngmit@outlook.com>
2016-05-28 20:31:28 -0400

    Fix bug in get_location_info

--- src/core/core_mesh.cpp
+++ src/core/core_mesh.cpp
@@ -267,7 +267,8 @@ CoreMesh::LocationInfo CoreMesh::get_location_info( Point3 p,
     info.reg_offset = first_reg_plane(info.pos.z);
 
     Point2 pin_origin = p.to_2d();
-    info.pm = planes_[info.pos.z].get_pinmesh(pin_origin, info.reg_offset, dir);
+    int plane_index = unique_planes_[info.pos.z];
+    info.pm = planes_[plane_index].get_pinmesh(pin_origin, info.reg_offset, dir);
     info.local_point -= pin_origin;
 
     return info;
--- src/core/plane.cpp
+++ src/core/plane.cpp
@@ -82,6 +82,8 @@ namespace mocc {
                 }
             }
         }
+
+        return;
     }
 
     /**
